eda05e6354355dda0a27e6ca706fef299f725bb0
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2019-08-28 22:15:42 -0300

    Include authors

--- init.sh
+++ init.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-AHTHORS="Rodrigo Vitor Ribeiro
-Lucas de Assis Dias
-Vinicius Vicentini
-Victor Vasconcellos
-Marcello Nardi"
+AUTHOR1="Rodrigo Vitor Ribeiro"
+AUTHOR2="Lucas de Assis Dias"
+AUTHOR3="Vinicius Vicentini"
+AUTHOR4="Victor Vasconcellos"
+AUTHOR5="Marcello Nardi"
 PROJECT="Lab. Eng. Software - Judocas web version"
 TARGET=./target
 JBOSS_HOME=$TARGET/EAP-7.2.0
@@ -19,3 +19,12 @@ PROJECT_GIT_REPO=https://github.com/victorborba7/Projeto-Lab-ES
 
 # clear screen.
 clear
+
+echo 
+echo "################################################################################"
+echo "## Projeto criado por                                                         ##"
+echo "##    ${AUTHOR1}                                                   ##"
+echo "##    ${AUTHOR2}                                                     ##"
+echo "##    ${AUTHOR3}                                                      ##"
+echo "##    ${AUTHOR4}                                                     ##"
+echo "##    ${AUTHOR5}                                                          ##"
