b11b5b9578a78056802458f560f186ae4c2d3d50
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2019-10-22 16:07:32 -0300

    Include validation of each command

--- add-datasource.sh
+++ add-datasource.sh
@@ -14,16 +14,36 @@ echo "Adding the module"
 echo
 $SERVER_BIN/jboss-cli.sh -c file=$SUPPORT_DIR/add-module.txt
 
+if [$? -ne 0] then
+	echo "Cannot create the module"
+	echo
+	exit
+
 echo "Adding the JDBC driver"
 echo
 $SERVER_BIN/jboss-cli.sh -c file=$SUPPORT_DIR/add-driver.txt
+
+if [ $? -ne 0] then
+	echo "Cannot create the driver"
+	echo
+	exit
 
 echo "Adding the datasource"
 echo
 $SERVER_BIN/jboss-cli.sh -c file=$SUPPORT_DIR/add-datasource.txt
 
+if [ $? -ne 0] then
+	echo "Cannot create the datasorce"
+	echo
+	exit
+
 echo "Testing the connection"
 echo
 $SERVER_BIN/jboss-cli.sh -f file=$SUPPORT_DIR/test-connection.txt
+
+if [ $? -ne 0] then
+	echo "Connection not validated"
+	echo
+	exit
 
 echo "Datasource configured..."
